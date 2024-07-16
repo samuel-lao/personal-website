ac4ee75cd83c92147c6f5b736466fdbd98018446
Samuel Lao <samuel.lao100@gmail.com>
2024-07-16 19:29:07 +0800

    continued working on project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import './App.css'
+
+import infer from '../assets/projects/infer.png'
 
 export default function Projects() {
   return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-        <div style={{backgroundColor: "red", width: "clamp(50rem, 70vw, 1200px)"}}>
-            Teddy
+    <div style={{display: "flex", justifyContent: "center", paddingTop: "5vh"}}>
+        <div style={{ width: "clamp(10vw, 70vw, 65rem)" }}>
+            <div className="category-container">
+                <div className="project-container">
+                    <img src={infer} height={"auto"} className='project-image'/>
+                    <text className="project-title">Infer!</text>
+                    <text className="project-subtitle">App to train inference skills of autistic audience using movies</text>
+                    <div className="tag-container">
+                        <text className="project-tag">ML (GEN TEXT)</text>
+                        <text className="project-tag">FULLSTACK</text>
+                        <text className="project-tag">REACT NATIVE</text>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
